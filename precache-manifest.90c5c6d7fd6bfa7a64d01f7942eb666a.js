6e305197b67ec6a10b0d4f75088730d7607b9a1c
hhh1361 <nikita.turlyko@gmail.com>
2019-10-10 12:23:49 +0300

    Updates

---
+++ precache-manifest.90c5c6d7fd6bfa7a64d01f7942eb666a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1cb011ca02ccf76dfff8a61fd417be5",
+    "url": "/youtube-client/index.html"
+  },
+  {
+    "revision": "4f6dc1dda56237f01f0d",
+    "url": "/youtube-client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "00f2013b784087afe7c9",
+    "url": "/youtube-client/static/js/2.58e1f502.chunk.js"
+  },
+  {
+    "revision": "4f6dc1dda56237f01f0d",
+    "url": "/youtube-client/static/js/main.f3ae56e4.chunk.js"
+  },
+  {
+    "revision": "e5bb901fafc11501685d",
+    "url": "/youtube-client/static/js/runtime-main.4bf1498a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/youtube-client/static/media/logo.25bf045c.svg"
+  }
+]);
